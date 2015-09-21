85f3ad5a6898b261452abc29f10d415a01334d68
Alexis Matelin <a.matelin@gmail.com>
2015-09-21 10:57:17 -0400

    Install dependencies and set-up app.js
    Imported newly added packages, added connection to the database and two middlewares : one to handle sessions and the other to modify POST requests

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+    path = require("path"),
+    favicon = require("serve-favicon"),
+    logger = require("morgan"),
+    cookieParser = require("cookie-parser"),
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    mongoose = require("mongoose"),
+    session = require("express-session"),
+    MongoStore = require("connect-mongo")(session);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Connect to mongodb
+mongoose.connect("mongodb://localhost/iotdb", function(err) {
+    if (err) throw err;
+    console.log("Successfully connected to mongodb");
+});
 
+// Load routes
+var routes = require("./routes/index");
+var users = require("./routes/users");
+
+// Start app
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+// We use mongodb to store session info
+// expiration of the session is set to 7 days (ttl option)
+app.use(session({
+    store: new MongoStore({mongooseConnection: mongoose.connection,
+                          ttl: 7*24*60*60}),
+    saveUninitialized: true,
+    resave: true,
+    secret: "MyBigBigSecret"
+}));
+ // used to manipulate post requests and recongize PUT and DELETE operations
+app.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === "object" && "_method" in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+}));
 
-app.use('/', routes);
-app.use('/users', users);
+// Register routes
+app.use("/", routes);
+app.use("/users", users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -36,10 +67,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -50,7 +81,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
