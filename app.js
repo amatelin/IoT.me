2cea1ebdb622785ecc17b06711efb630ebe753f5
Alexis Matelin <a.matelin@gmail.com>
2015-09-22 12:22:10 -0400

    Add front-end dependencies
    Added bootstrap, jQuery, glyphicons fonts, Chart.js for the plots, a logo and a favicon

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// Registering middlewares to the app
+app.use(favicon(path.join(__dirname, "public/images/favicon.ico")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
